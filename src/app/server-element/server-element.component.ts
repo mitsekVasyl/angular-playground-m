cafee3668fc222bcd960b3b16d6cfc9f31743156
mitsekVasyl <mitsek.vasyl@gmail.com>
2022-09-22 09:48:01 +0300

    View encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,  // None, ShadowDom
 })
 export class ServerElementComponent implements OnInit {
   // alias binding to parent component
