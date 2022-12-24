5e12be27df819a050721fa7a67cefdbefb194b61
mitsekVasyl <mitsek.vasyl@gmail.com>
2022-12-24 09:27:27 +0200

    User deactivation using UsersService

--- services_assignment/src/app/active-users/active-users.component.ts
+++ services_assignment/src/app/active-users/active-users.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UsersService } from "../services/Users.service";
 
 @Component({
   selector: 'app-active-users',
@@ -7,9 +8,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ActiveUsersComponent {
   @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersService) {
+  }
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.usersService.setToInactive(id);
   }
 }
