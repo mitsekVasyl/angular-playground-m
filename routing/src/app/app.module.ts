0eff900091c880eb2e508f401e7a93c407aa3790
mitsekVasyl <mitsek.vasyl@gmail.com>
2022-12-27 00:01:38 +0200

    Route parameter subscription to react on parameter update within the component

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'servers', component: ServersComponent },
 ]
 
--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route)
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: 'mock'
+    }
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    })
   }
 
 }
