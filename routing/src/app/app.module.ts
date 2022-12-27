64a3773cb3186af1bf7eb792afdb4a0b5f0b8e37
mitsekVasyl <mitsek.vasyl@gmail.com>
2022-12-27 21:38:11 +0200

    Nested routes with children components: users

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/:id', component: UserComponent},
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id', component: UserComponent}, ]
+  },
   {
     path: 'servers', component: ServersComponent, children: [
       {path: ':id', component: ServerComponent},
