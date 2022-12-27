75b35970afdf160ef7db30a13a0e9e5cb61b3309
mitsekVasyl <mitsek.vasyl@gmail.com>
2022-12-27 10:40:24 +0200

    Getting path param and a fragment

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 import { ServersService } from '../servers.service';
 
@@ -11,11 +12,24 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  serverId: number;
+  fragment: string;
+  queryParams: object;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // Retrieve query params, path param and fragment
+    this.queryParams = this.activatedRoute.snapshot.queryParams
+    this.serverId = this.activatedRoute.snapshot.params['id'];
+    this.fragment = this.activatedRoute.snapshot.fragment
+
+    // subscribe to params updates within the component
+    this.activatedRoute.params.subscribe((params) => this.serverId = params['id']);
+    this.activatedRoute.fragment.subscribe((fragment) => this.fragment = fragment);
+
+    this.server = this.serversService.getServer(this.serverId);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
