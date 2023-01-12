30697a8ec8e10912419d8b0e3311bd0b48408eb0
mitsekVasyl <mitsek.vasyl@gmail.com>
2023-01-12 21:36:51 +0200

    Passing form using ViewChild

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -7,11 +7,17 @@ import { NgForm } from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') form: NgForm
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
-    console.log(`form is submitted: ${form.value.username} ${form.value.email}`)
+  onSubmit() {
+    console.log(`form is submitted: ${this.form.value.username} ${this.form.value.email}`)
   }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(`form is submitted: ${form.value.username} ${form.value.email}`)
+  // }
 }
